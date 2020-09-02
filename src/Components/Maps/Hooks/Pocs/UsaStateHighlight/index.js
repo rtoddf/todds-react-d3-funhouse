154ad08a58c4ee1bfc33adffe8e7d873514f8944
rtoddf <rtoddf@me.com>
2020-09-02 11:55:49 -0400

    setting up highlights

--- src/Components/Maps/Hooks/Pocs/UsaStateHighlight/index.js
+++ src/Components/Maps/Hooks/Pocs/UsaStateHighlight/index.js
@@ -1,9 +1,10 @@
 import React, {useRef, useEffect, useState} from "react";
-import { select} from "d3";
+import { select, tsv } from "d3";
 import * as topojson from "topojson-client";
 import { geoAlbersUsa, geoPath } from "d3-geo";
 
 import us from '../../../../Data/topology/us.json';
+import info from '../../../../Data/info/us-states-locations.tsv';
 
 let width, height, vis_group, aspect
 
@@ -19,9 +20,28 @@ const defaults = {
     }
 }
 
+let stateData = {}
+// var data_set
+// var party_map = true,
+//     same_sex_map = false,
+//     age_of_consent_map = false
+
 function AlbersUsa02() {
     const [topology, setTopology] = useState({});
+    const [stateInfo, setStateInfo] = useState({});
     const svgRef = useRef();
+
+    function state_party_fill(d){
+        console.log('d.id: ', d)
+        // console.log('stateData: ', stateData)
+        // var location = stateData[d.id].location
+        // if(location == 'true'){
+        //     return '#e91d0e'
+        // } else {
+        //     return 'white'
+        // }
+        return 'red'
+    }
 
     useEffect(() => {
         const container_parent = document.querySelector('.display')
@@ -29,13 +49,26 @@ function AlbersUsa02() {
         width = container_parent.offsetWidth
         height = width * .5
 
-        Promise.all([us]).then(function(data) {
+        Promise.all([us, tsv(info)]).then(function(data) {
             setTopology(data[0])
+
+            data[1].forEach(function(d, i){
+                stateData[d.id] = {
+                    'name': d.name,
+                    'code': d.code,
+                    'party': d.location
+                }
+            })
+
+            setStateInfo(data[1])
         })
 
     }, [topology])
 
-    if(topology && topology.objects){
+    if(topology && topology.objects && stateInfo){
+        console.log('stateInfo: ', stateInfo)
+
+
         const svg = select(svgRef.current)
             .attrs({
                 'width': width + margins.left + margins.right,
@@ -62,12 +95,15 @@ function AlbersUsa02() {
             })
 
         vis_group.append('path')
-            .datum(topojson.mesh(topology, topology.objects.states, function(a, b){
+            .datum(topojson.feature(topology, topology.objects.states, function(a, b){
                 return a !== b
             }))
             .attrs({
                 'd': path,
-                'fill': 'none',
+                'fill': function(d){
+                    console.log('d in function: ', d)
+                    return state_party_fill(d.features)
+                },
                 'stroke': defaults.colors.stroke,
                 'stroke-width': defaults.colors.strokeWidth
             })
