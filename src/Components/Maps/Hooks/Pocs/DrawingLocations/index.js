d71c308fc096d080f193ace755078753c1911742
rtoddf <rtoddf@me.com>
2020-09-03 16:01:20 -0400

    all works, but getting double maps

--- src/Components/Maps/Hooks/Pocs/DrawingLocations/index.js
+++ src/Components/Maps/Hooks/Pocs/DrawingLocations/index.js
@@ -1,13 +1,15 @@
 import React, {useRef, useEffect, useState} from "react";
-import { select, tsv } from "d3";
+import * as d3 from "d3";
+import { select } from "d3";
 import * as topojson from "topojson-client";
 import { geoAlbersUsa, geoPath } from "d3-geo";
+import "./default.css";
 
 import us from '../../../../Data/topology/us.json';
-import info from '../../../../Data/info/us-states-locations.tsv';
-import locations from '../../../../Data/info/drawing-locations.json';
+import locations from '../../../../Data/info/us-state-locations.json';
 
-let width, height, vis_group, aspect
+let width, height, vis_group
+// aspect
 
 const margins = {top: 0, right: 20, bottom: 20, left: 20}
 const defaults = {
@@ -21,16 +23,18 @@ const defaults = {
 
 let stateData = {}
 
+var tooltip = select('body').append('div')
+    .attrs({
+        'class': 'tooltip'
+    })
+    .style({
+        'opacity': 1e-6
+    })
+
 function DrawingLocations() {
     const [topology, setTopology] = useState({});
     const [stateInfo, setStateInfo] = useState({});
     const svgRef = useRef();
-
-    function state_location_fill(d){
-        if(stateData[d.id]){
-            return stateData[d.id].location == 'true' ? defaults.colors.location : defaults.colors.land
-        }
-    }
 
     useEffect(() => {
         const container_parent = document.querySelector('.display')
@@ -38,19 +42,23 @@ function DrawingLocations() {
         width = container_parent.offsetWidth
         height = width * .5
 
-        Promise.all([us, tsv(info)]).then(function(data) {
+        Promise.all([us, locations]).then(function(data) {
             setTopology(data[0])
 
-            data[1].forEach(function(d, i){
+            // figure out how to take this out - it's not needed
+            data[1].forEach(function(d){
                 stateData[d.id] = {
                     'name': d.name,
                     'code': d.code,
-                    'location': d.location
+                    'location': d.location,
+                    'latitude': d.latitude,
+                    'longitude': d.longitude,
+                    'displayname': d.displayname
                 }
             })
 
-            setStateInfo(data[1])
-        })
+                setStateInfo(data[1])
+            })
 
     }, [topology])
 
@@ -71,21 +79,81 @@ function DrawingLocations() {
             .projection(projection);
 
         vis_group = svg.append('g')
+
         vis_group.selectAll('path')
             .data(topojson.feature(topology, topology.objects.states).features)
             .enter().append('path')
             .attrs({
                 'd': path,
-                'fill': d => state_location_fill(d),
+                'fill': (d) => stateData[d.id] && stateData[d.id].location ? defaults.colors.location : defaults.colors.land,
                 'stroke': defaults.colors.strokeColor,
                 'strokeWidth': defaults.colors.strokeWidth
+            })
+
+        vis_group.selectAll('circle')
+            .data(stateInfo)
+                .enter().append('circle')
+            .attrs({
+                'class': 'marker',
+                'cx': (d) => d.longitude != 0 && d.latitude != 0 ? projection([ d.longitude, d.latitude ])[0]: '',
+                'cy': (d) => d.longitude != 0 && d.latitude != 0 ? projection([ d.longitude, d.latitude ])[1]: '',
+                'r': 5,
+                'stroke': 'white'
+            })
+            .style('cursor', 'pointer')
+            .on('mouseover', function(d){
+                select('.tooltip')
+                    .html('<span>' + d.displayname + '</span>')
+                    .styles({
+                        'left': (d3.event.pageX) + 'px',
+                        'top': (d3.event.pageY - 35) + 'px'
+                    })
+                    .transition()
+                        .duration(500)
+                        .style('opacity', 1) 
+            })
+            .on('mouseout', function(d){
+                select('.tooltip')
+                    .transition()
+                        .duration(200)
+                        .style('opacity', 0) 
+            })
+            .on('click', function(d){
+                d3.select('.location-info-box')
+                    .style('display', 'block')
+    
+                d3.select('.location')
+                    .html(d.displayname)
+    
+                d3.select('.address')
+                    .html(d.address)
+    
+                d3.select('.email')
+                    .html(d.email)
+    
+                d3.select('.phone')
+                    .html(d.phone)
             })
     }
 
     return (
-        <div className="display">
-            <svg ref={svgRef} />
-        </div>
+        <div class="flex-container">
+			<div class="flex-row">
+                <div class="flex-item" data-weight="2">
+                    <div className="display">
+                        <svg ref={svgRef} />
+                    </div>
+                </div>
+                <div class="flex-item">
+                    <div className="location-info-box">
+                        <h2 className="location">location</h2>
+                        <h3 className="address">address</h3>
+                        <h6>e: <span className="email"></span></h6>
+                        <h6>p: <span className="phone"></span></h6>
+                    </div>
+                </div>
+            </div>
+        </div> 
     )
 }
 
